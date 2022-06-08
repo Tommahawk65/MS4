d46b9bb62af63949ea34a7ad90512b62619b901c
Tommahawk65 <tom.riggs@live.co.uk>
2022-06-08 18:59:03 +0000

    product url routing works

--- products/views.py
+++ products/views.py
@@ -22,12 +22,14 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
-    if request.user.is_authenticated:
-        user = UserProfile.objects.get(user=request.user)
-    else:
-        user = None
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+    
 

1a6f92b41729e3a45bb56fad9b9de5f3f695f7f8
Tommahawk65 <tom.riggs@live.co.uk>
2022-06-23 21:41:28 +0000

    aws setup

--- home/views.py
+++ home/views.py
@@ -2,21 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from products.models import Product
 
 
-# Create your views here.
-
 def index(request):
     """ A view to return the index page """
 
-    featured_product_1 = get_object_or_404(Product, pk=175)
-    featured_product_2 = get_object_or_404(Product, pk=44)
-    featured_product_3 = get_object_or_404(Product, pk=45)
+    #featured_product_1 = get_object_or_404(Product, pk=43)
+    #featured_product_2 = get_object_or_404(Product, pk=44)
+    #featured_product_3 = get_object_or_404(Product, pk=45)
 
-    template = 'home/index.html'
-    context = {
-        'featured_product_1': featured_product_1,
-        'featured_product_2': featured_product_2,
-        'featured_product_3': featured_product_3,
-    }
+    #context = {
+        #'featured_product_1': featured_product_1,
+       # 'featured_product_2': featured_product_2,
+        #'featured_product_3': featured_product_3,
+   # }
 
-    return render(request, template, context, 'home/index.html')
-
+    return render(request, 'home/index.html')
